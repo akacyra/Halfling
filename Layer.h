0b6adad39d3f3b824548319549897301e93c4bcc
Anthony Kacyra <akacyra@gmail.com>
2015-07-01 18:36:12 -0700

    Changed project directory structure.

---
+++ Layer.h
@@ -0,0 +1,91 @@
+#ifndef __LAYER_H__
+#define __LAYER_H__
+
+#include <vector>
+#include <string>
+#include "Util.h"
+#include "Color.h"
+
+class Console;
+
+class Layer
+{
+    friend Console;
+
+    public:
+    /*
+     * Width of the font to use, which determines cell size.
+     * Example: 
+     *    if full-width font is 12x12, then half-width would be 6x12
+     */
+        enum Width {
+            HalfWidth,
+            FullWidth
+        };
+
+    /* 
+     * Layer creation.
+     */
+        /*
+         * Construct and initialize layer.
+         * @cell_width: width of the font to use.
+         * @x,y: coordinates (in cells) of the top-left corner of the :ayer.
+         * @w,h: width (in cells) of the Layer.
+         *
+         * The Console's coordinate system is based off the smallest unit,
+         *  half width cells. So (x,y) for Layers should always be in half-
+         *  width cells. (w,h) are in the Layers own cell width.
+         */
+        Layer(Width cell_width, int x, int y, int w, int h);
+
+    /*
+     * Layer control operations.
+     */
+        // Clears the Layer using default foreground and background colors.
+        void clear();
+        // Adds ch to the Layer at position (x,y) using default colors.
+        void put_char(int x, int y, char ch);
+        // Adds ch to the Layer at position (x,y) width specified colors.
+        void put_char(int x, int y, char ch, Color fg, Color bg);
+        // Adds str to the Layer starting at position (x,y) using default colors.
+        void put_str(int x, int y, std::string str);
+        // Adds str to the Layer starting at position (x,y) width specified colors.
+        void put_str(int x, int y, std::string str, Color fg, Color bg);
+        // Returns the char, fg color, and bg color of the cell at position (x,y).
+        std::tuple< char, Color, Color > operator()(int x, int y) const;
+
+    /*
+     * Layer settings operations.
+     */
+        // Returns the cell width of the Layer.
+        Width cell_width() const;
+        // Sets the default foreground color.
+        void set_fg_color(Color id);
+        // Sets the default background color.
+        void set_bg_color(Color id);
+        // Returns the default foreground color id.
+        Color get_fg_color() const;
+        // Returns the default background color id.
+        Color get_bg_color() const;
+
+    private:
+        Console* parent;
+
+        struct Cell {
+            char ch;
+            Color fg, bg;
+        };
+
+        const Cell& cell_at(int x, int y) const;
+        Cell& cell_at(int x, int y);
+
+        typedef std::vector< std::vector < Cell > > Buffer;
+        Buffer frontbuffer, backbuffer;
+
+        Rect bounds;
+        Width cell_w;
+
+        Color default_fg, default_bg;
+};
+
+#endif
